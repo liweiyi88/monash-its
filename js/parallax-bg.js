4781b3d65fca146d241dfc6507fb1030300bb29e
Jmily <weiyi.li713@gmail.com>
2014-09-29 16:24:50 +1000

    personal website for Majid

---
+++ js/parallax-bg.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	//Home Image Slider
+	$.backstretch([
+	  "./images/slide_1.jpg",
+	  "./images/slide_2.jpg",
+	  "./images/slide_3.jpg"    
+	], {duration: 4500, fade: 'slow'});
+	
+});
